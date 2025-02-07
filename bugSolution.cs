92b8b1e552a854cc532b261113cd837c761d06af
BugHunter-X <askzenai@gmail.com>
2025-02-07 08:32:06 +0100

    Add bugSolution.cs file

---
+++ bugSolution.cs
@@ -0,0 +1,14 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; }
+
+    public void MyMethod()
+    {
+        int i = 0;
+        while (i < 10)
+        {
+            //Increment the variable i to terminate the loop.
+            i++;
+        }
+    }
+}
